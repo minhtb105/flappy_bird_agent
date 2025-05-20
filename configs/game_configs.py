0c21263d1d63dfbcebf2d00b6eb95fb6036a3738
minhtb105 <tranbinhminh_t64@hus.edu.vn>
2025-05-20 09:12:42 +0700

    increase gravity, reduce num rays

--- configs/game_configs.py
+++ configs/game_configs.py
@@ -6,7 +6,7 @@ YELLOW = (255, 255, 0)
 
 # Pygame setup
 WIDTH = 512
-HEIGHT = 512
+HEIGHT = 412
 FPS = 60
 
 # Background
@@ -17,24 +17,23 @@ BASE_HEIGHT = HEIGHT - BACKGROUND_HEIGHT
 
 # Bird properties
 BIRD_X = 24
-BIRD_Y = 300
+BIRD_Y = BACKGROUND_HEIGHT / 2
 BIRD_WIDTH = 34
 BIRD_HEIGHT = 24
-GRAVITY = 0.5
-JUMP_STRENGTH = -8  # Apply a jump force to the bird (negative value makes it move upwards)
-BIRD_ANGLE = 0
+GRAVITY = 1.6
+JUMP_STRENGTH = -6  # Apply a jump force to the bird (negative value makes it move upwards)
 
 # Pipe properties
 PIPE_WIDTH = 52
 PIPE_SPEED = 3
 PIPE_GAP_SIZE = 100  # Distance between top pipe and bottom pipe
-PIPE_START_OFFSET = 250 # Offset for the first pipe
+PIPE_START_OFFSET = 350 # Offset for the first pipe
 PIPE_SPACING = 200
 NUM_PIPES = 2  # Number of pipes on the screen at a time
 
-NUM_RAYS = 180
-MAX_RAY_LENGTH = 50
-PLAYER_PRIVATE_ZONE = 26  # Size of agent's private zone
+NUM_RAYS = 60
+MAX_RAY_LENGTH = 0.8 * WIDTH - BIRD_X  # Maximum length of the ray
+PLAYER_PRIVATE_ZONE = 30  # Size of agent's private zone
 
 # Asset paths
 ASSETS_PATH = "assets/"
